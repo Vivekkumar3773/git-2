2fb7fd0334142512cba5251415af1c241a479a2f
Vivekkumar3773 <vivekrocky5223@gmail.com>
2025-01-03 20:14:33 +0530

    This my day 3 of 365 days of coding

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const addProductRoutes = require('./routes/add-product');
 const productRoutes = require('./routes/products');
 const errorRoutes = require('./routes/404');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname)));
 
 app.use(addProductRoutes);
 app.use(productRoutes);
--- routes/products.js
+++ routes/products.js
@@ -3,7 +3,7 @@ const express = require('express');
 const products = express.Router();
 
 products.get('/', (req, res, next) => {
-    res.render('product-list', { pageTitle: 'Product-Page', path: '/' });
+    res.render('product-list', { pageTitle: 'Product-Page', path: '/', prods: products });
 })
 
 module.exports = products;
